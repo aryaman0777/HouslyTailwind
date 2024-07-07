a79fffdeddf43a4321e91b3e6ecf33fd0b13523c
aryaman0777 <aryamanpd.bkp@gmail.com>
2024-07-07 21:36:05 +0530

    first commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+//JavaScript to handle the mobile menu toggle
+document.getElementById('nav-toggle').addEventListener('click', function () {
+    var navMenu = document.getElementById('nav-menu');
+    navMenu.classList.toggle('hidden');
+});
+
